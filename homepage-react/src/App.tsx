3607c0b7d1e4b851283f5752caa4ae0482045e70
ErnandesCosta <jegc@discente.ifpe.edu.br>
2026-02-09 23:16:47 -0300

    Preparando o ambiente para desenvolvimento
    configurando em tailwind para estilização

---
+++ homepage-react/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind v4 funcionando
+      </h1>
+    </div>
+  );
+}
+
+export default App;
